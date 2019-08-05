558bf739d9d56c739633acb89b23b7bd93031491
Alex <sanya112001@yandex.ru>
2019-08-05 19:56:38 +0300

    Add navigation drawer

--- app/src/main/java/com/washinson/yaradio3/MainActivity.kt
+++ app/src/main/java/com/washinson/yaradio3/MainActivity.kt
@@ -1,44 +1,56 @@
 package com.washinson.yaradio3
 
-import android.net.Uri
 import android.os.Bundle
-import android.text.TextUtils.replace
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBarDrawerToggle
 import android.view.MenuItem
-import android.widget.FrameLayout
-import android.widget.Toast
-import com.washinson.yaradio3.Session.Session
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import android.view.Menu
 
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.concurrent.thread
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-class MainActivity : AppCompatActivity(), TagsFragment.OnFragmentInteractionListener {
 
-    var session: Session? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        thread {
-            //session = Session(this)
-            //supportFragmentManager.beginTransaction()
-            //    .replace(R.id.tags_frame, TagsFragment.newInstance()).commit()
+        navView.setNavigationItemSelectedListener(this)
+
+        supportFragmentManager.beginTransaction().replace(R.id.tags_frame, LoadingFragment()).commit()
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.tags_frame, LoadingFragment.newInstance()).commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
@@ -50,5 +62,15 @@ class MainActivity : AppCompatActivity(), TagsFragment.OnFragmentInteractionList
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.type_users -> {}
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
